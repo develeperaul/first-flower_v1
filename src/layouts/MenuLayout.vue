5ad7bf4ac36229264c35b7d220df260373607962
Ralfik <developeraul@gmail.com>
2021-07-16 16:05:36 +0500

    убрал жирность поставил на флексы

--- src/layouts/MenuLayout.vue
+++ src/layouts/MenuLayout.vue
@@ -39,44 +39,51 @@
         behavior="desktop"
         :width="width"
         :mini-width="300"
-        content-class="gradient-green tw-pt-6 "
+        content-class="gradient-green tw-pt-6 
+        tw-flex tw-flex-col tw-justify-between
+        "
       >
-        <div v-if="!title" class="tw-text-center tw-mb-3.5 tw-pl-4" >
-          <span class="tw-font-semibold">Категории</span>
-        </div>
-        <div v-else class="tw-text-center tw-mb-3.5 tw-relative  p-content " >
-          <q-icon
-            @click="getParent"
-            name="arrow_back_ios"
-            class="tw-absolute tw-left-0 tw-top-1/2 tw-transform tw--translate-y-1/2 tw-pl-7 "
-          ></q-icon>
-          <span class="tw-font-semibold">{{title}}</span>
-        </div>
-        <div v-for="(item,index) in list" :key="index" @click="getChildren(item) " >
-          <div class="tw-flex tw-justify-between tw-items-center p-content tw-py-5 tw-ml-4">
-            <span class="tw-font-medium" >{{item.name}}</span>
-            <q-icon
-              v-if="!title"
-              name="arrow_forward_ios"
-              class=""
-            />
+
+          <div>
+
+          
+            <div v-if="!title" class="tw-text-center tw-mb-3.5 tw-pl-4" >
+              <span class="tw-font-semibold">Категории</span>
+            </div>
+            <div v-else class="tw-text-center tw-mb-3.5 tw-relative  p-content " >
+              <q-icon
+                @click="getParent"
+                name="arrow_back_ios"
+                class="tw-absolute tw-left-0 tw-top-1/2 tw-transform tw--translate-y-1/2 tw-pl-7 "
+              ></q-icon>
+              <span class="tw-font-semibold">{{title}}</span>
+            </div>
+            <div v-for="(item,index) in list" :key="index" @click="getChildren(item) " >
+              <div class="tw-flex tw-justify-between tw-items-center p-content tw-py-5 tw-ml-4">
+                <span class="tw-font-medium" >{{item.name}}</span>
+                <q-icon
+                  v-if="!title"
+                  name="arrow_forward_ios"
+                  class=""
+                />
+              </div>
+              <BorderLine/>
+            </div>
           </div>
-          <BorderLine/>
-        </div>
-        <div class="tw-flex tw-flex-col tw-mt-16 tw-mb-6  p-content ">
-          <span class="tw-font-semibold tw-text-lg">
-            Контакты магазина
-          </span>
-          <span class="tw-font-semibold tw-pt-3.5 tw-flex">
-            <Icon name="phone"  class="tw-mr-3"/>
-            896273454999
-          </span>
-          <span class="tw-font-semibold tw-pt-3.5 tw-flex">
-            <Icon name="location" class="tw-mr-3"/>
-            Чишмы,&nbsp;ул.&nbsp;Ленина,&nbsp;31
-          </span>
-        </div>
-        
+          <div class="tw-flex tw-flex-col  p-content tw-mt-3 tw-mb-12  tw-ml-4">
+            <span class="tw-font-semibold tw-text-lg">
+              Контакты магазина
+            </span>
+            <span class="tw-font-medium tw-pt-3.5 tw-flex">
+              <Icon name="phone"  class="tw-mr-3"/>
+              896273454999
+            </span>
+            <span class="tw-font-medium tw-pt-3.5 tw-flex">
+              <Icon name="location" class="tw-mr-3"/>
+              Чишмы,&nbsp;ул.&nbsp;Ленина,&nbsp;31
+            </span>
+          </div>
+       
       </q-drawer>
       <q-page-container >
         <router-view class="tw-my-2.5" />
